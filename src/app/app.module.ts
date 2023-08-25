35b9cf4a8198be32d8f19e185d5847d697e3fce5
RAVEENDRA KATTUBADI <raveendrakattubadi@gmail.com>
2023-08-26 00:01:05 +0530

    first_cahnges

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './componets/home/home.component';
+import { HeaderComponent } from './componets/header/header.component';
+import { FooterComponent } from './componets/footer/footer.component';
+import { LoginComponent } from './componets/login/login.component';
+import { AboutComponent } from './componets/about/about.component';
+import { ContactComponent } from './componets/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
